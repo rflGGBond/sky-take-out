2094ded8b403e8db021d76036fe282a55cbd7b14
rflGGBond <3357253246@qq.com>
2025-02-01 15:11:04 +0800

    苍穹外卖项目day6：导入商品浏览功能

---
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
@@ -0,0 +1,54 @@
+package com.sky.interceptor;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.context.BaseContext;
+import com.sky.properties.JwtProperties;
+import com.sky.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.HandlerMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@Slf4j
+public class JwtTokenUserInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 校验jwt
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        // 1、从请求头中获取令牌
+        String token = request.getHeader(jwtProperties.getUserTokenName());
+        // 2、校验令牌
+        try {
+            log.info("jwt校验:{}", token);
+            Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
+            Long userId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
+            log.info("当前用户id：", userId);
+            BaseContext.setCurrentId(userId);
+            return true;
+        } catch (Exception ex) {
+            // 3、如果校验失败，返回401状态码
+            response.setStatus(401);
+            return false;
+        }
+    }
+}
