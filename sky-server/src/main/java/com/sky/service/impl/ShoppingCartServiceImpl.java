5178389cf5895a419408d5f159d09c949b9c2ea2
rflGGBond <3357253246@qq.com>
2025-02-02 11:23:42 +0800

    苍穹外卖项目day7：查看购物车

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -59,5 +60,13 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCart.setCreateTime(LocalDateTime.now());
             shoppingCartMapper.insert(shoppingCart);
         }
+    }
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        return shoppingCartMapper.list(ShoppingCart
+                .builder()
+                .userId(BaseContext.getCurrentId())
+                .build());
     }
 }
